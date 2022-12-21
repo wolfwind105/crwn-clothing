0027634b5a95c7419d8fb7dcf25fa7224fe19286
Ravey <zxcv7879105@gmail.com>
2022-12-22 02:47:04 +0800

    新增結帳頁面,購物車功能

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,9 +1,17 @@
-import './product-card.styles.scss'
+import { useContext } from 'react';
 
 import Button from '../button/button.component'
+import { CartContext } from '../../contexts/cart.context';
+
+import './product-card.styles.scss'
+
 
 const ProductCard = ({ product }) => {
     const { name, price, imageUrl } = product;
+    const { addItemToCart } = useContext(CartContext);
+
+    const addProductToCart = () => addItemToCart(product)
+
     return (
     <div className="product-card-container">
         <img src={imageUrl} alt={`${name}`} />
@@ -11,7 +19,7 @@ const ProductCard = ({ product }) => {
             <span className='name'>{name}</span>
             <span className='price'>{price}</span>
         </div>
-        <Button buttonType='inverted'>新增商品</Button>
+        <Button buttonType='inverted' onClick={addProductToCart}>新增商品</Button>
     </div>
     );
 };
