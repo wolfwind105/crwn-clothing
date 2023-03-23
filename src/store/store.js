c47fe1565a5d7ba764c21fc33c17bbaef138ed49
Ravey <zxcv7879105@gmail.com>
2023-03-24 05:12:15 +0800

    導入 Redux-Presist, Redux-Thunk 試用Redux-Devtools

--- src/store/store.js
+++ src/store/store.js
@@ -1,24 +1,29 @@
 import { compose, createStore, applyMiddleware } from 'redux';
-// import logger from 'redux-logger';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 
 import { rootReducer } from './root-reducer';
 
-const loggerMiddleware = (store) => (next) => (action) => {
-  if (!action.type) {
-    return next(action);
-  }
 
-  console.log('type: ', action.type);
-  console.log('payload: ', action.payload);
-  console.log('currentState: ', store.getState());
 
-  next(action);
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart'],
+}
 
-  console.log('next state: ', store.getState());
-};
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
-const middleWares = [loggerMiddleware];
+const middleWares = [
+  process.env.NODE_ENV !== 'production' && logger,
+  thunk
+].filter(Boolean);
 
-const composedEnhancers = compose(applyMiddleware(...middleWares));
+const composedEnhancer = (process.env.NODE_ENV !== 'production' && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+const composedEnhancers = composedEnhancer(applyMiddleware(...middleWares));
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+export const store = createStore(persistedReducer, undefined, composedEnhancers);
+
+export const persistor = persistStore(store);
